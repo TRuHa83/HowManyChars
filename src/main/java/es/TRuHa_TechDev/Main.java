2db746881683eb230d34f17042586cad0e62e889
Sergio Trujillo <sergiotrujillonuez@gmail.com>
2025-01-15 23:57:11 +0100

    Obtención de datos y control de excepciones.

--- src/main/java/es/TRuHa_TechDev/Main.java
+++ src/main/java/es/TRuHa_TechDev/Main.java
@@ -1,20 +1,51 @@
 package es.TRuHa_TechDev;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+
+import java.util.Random;
 import java.util.Scanner;
 
+
 public class Main {
-    private static final String workpath = System.getProperty("user.dir") + "/";
+    private static final String workpath = System.getProperty("user.dir");
     private static final String fileRepeat = "repetir.txt";
     private static final String fileDestiny = "destino.txt";
     private static final String fileError = "error.txt";
+
+    public static void genRepeat() {
+        System.out.println("[S] Generando datos...");
+
+        Random ram = new Random();
+        String text = "";
+
+        int count = ram.nextInt(1, 20);
+        for (int i = 1; i <= count; i++) {
+            int num = ram.nextInt(1, 20);
+            int let = ram.nextInt(65, 90);
+
+            String line = String.format("%s;%s",num, (char) let);
+            text += line + "\n";
+
+            String symbol = "├";
+            if (i == count) symbol = "└";
+
+            System.out.printf(" %s[%s] %s\n", symbol, i, line);
+
+        }
+
+        Write(fileRepeat, text);
+
+    }
 
     public static String Read(String filename) {
         String text = "";
 
         try {
-            FileReader file = new FileReader(workpath + filename);
+            // para compatibilizar con sistemas Linux y Windows
+            String pathfile = workpath + File.separator + filename;
+            FileReader file = new FileReader(pathfile);
 
             int chart = file.read();
 
@@ -25,13 +56,15 @@ public class Main {
                 chart = file.read();
             }
 
+            file.close();
+
         } catch (IOException _) {}
 
         return text;
     }
 
-    public static void Write() {
-
+    public static void Write(String filename, String data) {
+        System.out.printf("Guardando datos en el archivo: %s\n", filename);
     }
 
     public static void main(String[] args) {
@@ -40,16 +73,16 @@ public class Main {
         if (repeat.isEmpty()) {
             Scanner input = new Scanner(System.in);
 
-            System.out.println("[!] El archivo está vacío");
-            System.out.print("[?] ¿Generar aleatorio? [S/N]");
+            System.out.println("[!] El archivo está vacío o no existe");
+            System.out.print("[?] ¿Generar aleatorio? [S/N] ");
             String option = input.nextLine().toLowerCase();
 
             if (option.equals("s")) {
-                System.out.println("[S] Generando archivo...");
+                genRepeat();
 
             } else  {
                 System.out.println("[N] Se necesitan datos para continuar :(");
-                System.exit(0);
+                System.exit(1);
             }
 
         } else {
